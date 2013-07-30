cbdcd0532c9d541f34076cd9bcd56ae5605760ca
root <root@pentester.lan>
2013-07-30 11:00:59 +0100

    formatting

--- handshaker.sh
+++ handshaker.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+## HandShaker Copyright 2013, d4rkcat (d4rkc4t@tormail.org)
+#
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+#
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License at (http://www.gnu.org/licenses/) for
+## more details.
+
+
 fapscan()
 {
 		clear
